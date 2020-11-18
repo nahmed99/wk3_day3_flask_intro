77d0f86524fbe3c361617251f725716d05f07b3d
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-18 16:51:39 +0000

    some flask intro work, and comments advising how to run flask server...

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 # run the server
 app = Flask(__name__)
 
+import controller
+
 # launch server
 if __name__ == '__main__':
     app.run(debug=True)
--- controller.py
+++ controller.py
@@ -3,3 +3,20 @@ from app import app
 @app.route('/')  # This is a HTTP route
 def index():
     return "Hello, world"
+
+# To run the server...
+# To run the app.py in flask, type in the following lines:
+# export FLASK_APP=app.py
+# flask run
+
+# http://127.0.0.1 is the local host, so you could use 
+# localhost:5000 instead of http://127.0.0.1:5000
+
+# <ctrl>-C to stop the serve
+
+
+# localhost:5000/name_passed_in
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello, {name}"
+
