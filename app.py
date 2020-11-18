34766a968f7c0b8f8ceba5c0fe0cf2e016d7f15b
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-18 17:17:31 +0000

    Intro to Flask and passing parms in url...

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
-# run the server
+# To run the server...
+# To run the app.py in flask, type in the following lines:
+# export FLASK_APP=app.py
+# flask run
+
+# http://127.0.0.1 is the local host, so you could use 
+# localhost:5000 instead of http://127.0.0.1:5000
+
+# <ctrl>-C to stop the serve
+
+
+# create an instance of Flask
 app = Flask(__name__)
 
 import controller
 
-# launch server
+# 
 if __name__ == '__main__':
     app.run(debug=True)
--- controller.py
+++ controller.py
@@ -1,6 +1,16 @@
-from app import app
+from app import app  #  we are importing the Flask instance called app from the file called app.py.
 
-@app.route('/')  # This is a HTTP route
+'''
+Routes are the different URLs that the application implements. In Flask, handlers for the routes are written as Python functions, called view functions.
+
+controller.py will deal with requests that come in to the application and determine what view functions will run based on the url that the user requests.
+
+
+'''
+
+# Decorators add extra functionality to a function in Python. The @app.route decorator creates an association between the URL and the function.
+# In this example the decorator associates the URL / to this function. This means that when a web browser requests this URL, Flask is going to execute this function and pass the return value of it back to the browser as a response. 
+@app.route('/')  # This is a HTTP route "decorator".
 def index():
     return "Hello, world"
 
@@ -14,9 +24,11 @@ def index():
 
 # <ctrl>-C to stop the serve
 
-
-# localhost:5000/name_passed_in
-@app.route('/hello/<name>')
+# create a new route that will accept a paramter from the (browser) url.
+# localhost:5000/name_passed_in (name_passed_in will be displayed after hello...)
+@app.route('/<name>')
 def hello(name):
     return f"Hello, {name}"
 
+
+# NOTE: Regardless of the name we use in the url we always hit the greet route and the name is used to build up the string.
