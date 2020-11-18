9c499bf9faaec9d773cf83253c4a65ac62d191fe
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-18 16:17:12 +0000

    Intro to Flask

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
+# run the server
 app = Flask(__name__)
 
+# launch server
 if __name__ == '__main__':
     app.run(debug=True)
--- controller.py
+++ controller.py
@@ -1,1 +1,5 @@
 from app import app
+
+@app.route('/')  # This is a HTTP route
+def index():
+    return "Hello, world"
